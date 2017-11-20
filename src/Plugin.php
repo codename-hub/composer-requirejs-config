1c0b08acda1ae3aa2221f2414655ea62c2326abb
Kevin Dargel <kevin@jocoon.de>
2017-11-20 09:51:46 +0100

    improved the plugin. now supports aliases and jspm config override generates shim config on need

--- src/Plugin.php
+++ src/Plugin.php
@@ -23,14 +23,15 @@ class Plugin implements PluginInterface, \Composer\EventDispatcher\EventSubscrib
    */
   protected $io;
 
+  /**
+   * [activate description]
+   * @param  Composer    $composer [description]
+   * @param  IOInterface $io       [description]
+   * @return [type]                [description]
+   */
 	public function activate( Composer $composer, IOInterface $io ) {
     $this->composer = $composer;
     $this->io = $io;
-
-    // $io->write("helo");
-
-		// $installer = new Generator( $io, $composer );
-		// $composer->getInstallationManager()->addInstaller( $installer );
 	}
 
   /**
@@ -52,10 +53,6 @@ class Plugin implements PluginInterface, \Composer\EventDispatcher\EventSubscrib
 
     // provides access to the current Composer instance
     $composer = $event->getComposer();
-    // run any post install tasks here
-
-    // debug.
-    // $this->io->write("Got composer.");
 
     //.. do stuff!
     $packageTypes = array();
@@ -75,12 +72,11 @@ class Plugin implements PluginInterface, \Composer\EventDispatcher\EventSubscrib
 
     foreach($config as $configElement) {
 
-      $this->io->write("Config Element");
-
       $generateFile = null;
       $basePath = null;
       $baseUrl = null;
       $types = array();
+      $alias = array();
 
   		if ( !empty( $configElement['generate-file'] ) ) {
   			$generateFile = $configElement['generate-file'];
@@ -105,6 +101,11 @@ class Plugin implements PluginInterface, \Composer\EventDispatcher\EventSubscrib
         continue;
       }
 
+      // defined package aliases
+      if ( !empty( $configElement['alias'] ) ) {
+  			$alias = $configElement['alias'];
+  		}
+
       $data = array(
         'paths' => array()
       );
@@ -119,50 +120,81 @@ class Plugin implements PluginInterface, \Composer\EventDispatcher\EventSubscrib
         if(in_array($package->getType(), $types)) {
           $json = $this->getPackageJson($package);
 
+          $name = $alias[$json['name']] ?? $json['name'];
+
           // we have a package
           if($json !== null) {
-            // handle the "main" key
-            if(isset($json['main'])) {
 
-              $pathInfo = pathinfo($json['main']);
-              $installPath = $this->composer->getInstallationManager()->getInstallPath($package);
+            $main = null;
+            $shim = null;
 
-              // the real main path (without ext (?) )
-              $main = $pathInfo['dirname'] . '/' . $pathInfo['filename'];
+            $main = $json['main'] ?? null;
 
-              $this->io->write("Name: " . $json['name']);
-              $this->io->write("InstallPath: " . $installPath);
-              $this->io->write("Main: " . $main);
+            //
+            // browserify??
+            //
+            // if(!empty($json['browser']) && !empty($json['browser']['main'])) {
+            //   $main = $json['browser']['main'];
+            // }
 
-              if(strpos($installPath, $basePath) === 0) {
-                // Strip down to relative path (basePath)
-                $relPath = str_replace($basePath, '', $installPath);
-                $this->io->write("RelPath: " .$relPath);
+            //
+            // jam js
+            //
+            // if(!empty($json['jam']) && !empty($json['jam']['main'])) {
+            //   $main = $json['jam']['main'];
+            // }
 
-                $virtualPath = self::normalizePath($relPath.$main);
+            //
+            // jspm js
+            //
+            if(!empty($json['jspm']) && !empty($json['jspm']['main'])) {
 
-                $this->io->write("Value: " .$virtualPath );
+              // default directory base for jspm shim is dist (?)
+              $jspmBase = 'dist/';
 
-                $data['paths'][$json['name']] = $virtualPath;
+              $main = $jspmBase . $json['jspm']['main'] ?? null;
+              $jspmShim = $json['jspm']['shim'] ?? null;
+
+              if($jspmShim) {
+                // modify shim
+                foreach($jspmShim as $shimElement => $shimConfig) {
+                  if($shimElement == $json['jspm']['main']) {
+                    // we got the right one
+                    $shim = $shimConfig;
+                    // jspm supports deps as a string (single dep) - requirejs doesn't!
+                    if(isset($shim['deps']) && !is_array($shim['deps'])) {
+                      $shim['deps'] = array($shim['deps']);
+                    }
+                  }
+                }
+
               }
+            }
 
-              // TODO: strip out . ? convert to real path?
-              // path > name => path to main js without '.js' (filename)
-              //$data['paths'][$json['name']] = $installPath . $pathInfo['dirname'] . '/' . $pathInfo['filename'];
+            $pathInfo = pathinfo($main);
 
-              $this->io->write("");
+            $installPath = $this->composer->getInstallationManager()->getInstallPath($package);
 
-            } else {
-              // no main key!
+            // the real main path (without ext (?) )
+            $requireName = $pathInfo['dirname'] . '/' . $pathInfo['filename'];
+
+            if(strpos($installPath, $basePath) === 0) {
+              // Strip down to relative path (basePath)
+              $relPath = str_replace($basePath, '', $installPath);
+              $virtualPath = self::normalizePath($relPath.$requireName);
+              $data['paths'][$name] = $virtualPath;
+
+              // shim on need:
+              if($shim) {
+                $data['shim'][$name] = $shim;
+              }
             }
           }
         }
       }
 
-
       // write the config!
       $this->writeRequireConfigJs($generateFile, $data);
-
     }
   }
 
@@ -173,9 +205,7 @@ class Plugin implements PluginInterface, \Composer\EventDispatcher\EventSubscrib
    */
   protected function writeRequireConfigJs(string $file, array $config) {
     $content = "require.config(". json_encode($config, JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES) . ");";
-
-    $this->io->write("Would write (" .$file. "):\n" . $content);
-
+    $this->io->write("Writing requireJS config file (" .$file. "):\n" . $content);
     file_put_contents($file, $content);
   }
 
@@ -185,16 +215,14 @@ class Plugin implements PluginInterface, \Composer\EventDispatcher\EventSubscrib
    */
   protected function getPackageJson(\Composer\Package\PackageInterface $package) {
     // retrieves the NPM Package.json in install dir, if available
-
     $installPath = $this->composer->getInstallationManager()->getInstallPath($package);
     $packageJsonFile = $installPath . 'package.json';
     if(file_exists($packageJsonFile)) {
       $packageJson = file_get_contents($packageJsonFile);
       $decoded = json_decode($packageJson, true);
       return $decoded;
-      // echo("main file: " . $decoded['main'] . chr(10));
     } else {
-      // echo("no json");
+      // No JSON
     }
     return null;
   }
@@ -204,34 +232,34 @@ class Plugin implements PluginInterface, \Composer\EventDispatcher\EventSubscrib
    */
   protected static function normalizePath($path)
   {
-      $parts = array();// Array to build a new path from the good parts
-      $path = str_replace('\\', '/', $path);// Replace backslashes with forwardslashes
-      $path = preg_replace('/\/+/', '/', $path);// Combine multiple slashes into a single slash
-      $segments = explode('/', $path);// Collect path segments
-      $test = '';// Initialize testing variable
-      foreach($segments as $segment)
-      {
-          if($segment != '.')
-          {
-              $test = array_pop($parts);
-              if(is_null($test))
-                  $parts[] = $segment;
-              else if($segment == '..')
-              {
-                  if($test == '..')
-                      $parts[] = $test;
+    $parts = array();// Array to build a new path from the good parts
+    $path = str_replace('\\', '/', $path);// Replace backslashes with forwardslashes
+    $path = preg_replace('/\/+/', '/', $path);// Combine multiple slashes into a single slash
+    $segments = explode('/', $path);// Collect path segments
+    $test = '';// Initialize testing variable
+    foreach($segments as $segment)
+    {
+        if($segment != '.')
+        {
+            $test = array_pop($parts);
+            if(is_null($test))
+                $parts[] = $segment;
+            else if($segment == '..')
+            {
+                if($test == '..')
+                    $parts[] = $test;
 
-                  if($test == '..' || $test == '')
-                      $parts[] = $segment;
-              }
-              else
-              {
-                  $parts[] = $test;
-                  $parts[] = $segment;
-              }
-          }
-      }
-      return implode('/', $parts);
+                if($test == '..' || $test == '')
+                    $parts[] = $segment;
+            }
+            else
+            {
+                $parts[] = $test;
+                $parts[] = $segment;
+            }
+        }
+    }
+    return implode('/', $parts);
   }
 
 
